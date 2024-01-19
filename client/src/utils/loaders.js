04aeb49fc6dc985b91f48451f6292655081f14c2
NyashaDZT <nyashapatrickdzvoti@live.co.uk>
2024-01-19 09:57:51 +0000

    Friday, react setup

---
+++ client/src/utils/loaders.js
@@ -0,0 +1,36 @@
+import { getToken } from "./helpers/common"
+import axios from "axios"
+
+export async function quizLoader() {
+  const res = await fetch(`/api/quizzes`)
+  if (!res.ok) {
+    throw new Error(`Failed to fetch quizzes: ${res.statusText}`);
+  }
+  return res.json()
+}
+
+export async function singleQuizLoader(quizId) {
+  const res = await fetch(`/api/houses/${quizId}`)
+  if (!res.ok) {
+    throw new Error(`Failed to fetch quiz: ${res.statusText}`);
+  }
+  return res.json()
+}
+
+export async function singleQuestionLoader(questionId) {
+  const res = await fetch(`/api/houses/${questionId}`)
+  if (!res.ok) {
+    throw new Error(`Failed to fetch question: ${res.statusText}`);
+  }
+  return res.json()
+}
+
+export async function profileLoader() {
+  const res = await axios.get(`/api/users`, {
+    validateStatus: () => true,
+    headers: {
+        Authorization: `Bearer ${getToken()}`
+    }
+}) 
+  return res.data
+}
