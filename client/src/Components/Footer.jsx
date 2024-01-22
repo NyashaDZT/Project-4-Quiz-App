5da4b074230ba4a1dd1e7aa1a7eaee882343b294
NyashaDZT <nyashapatrickdzvoti@live.co.uk>
2024-01-22 11:26:08 +0000

    footer styled

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,10 +1,21 @@
+
+const footerStyle = {
+  backgroundColor: 'black',
+  color: 'white',
+  padding: '10px',
+  textAlign: 'center',
+  position: 'fixed',
+  bottom: '0',
+  width: '100%',
+};
+
 export default function Footer() {
   return (
-    <footer style={{ backgroundColor: 'black', color: 'white', padding: '10px', textAlign: 'center' }}>
-      <p style={{ margin: 0 }}>Created by Nyasha Dzvoti</p>
-      <a href="https://github.com/NyashaDZT" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}>
-        GitHub Profile
+    <div style={footerStyle}>
+      <p>Created by Nyasha Dzvoti</p>
+      <a href="https://github.com/NyashaDZT" target="_blank" rel="noopener noreferrer">
+        Visit my GitHub
       </a>
-    </footer>
-  )
+    </div>
+  );
 }
